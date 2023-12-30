d268a7fbab17d1c1df5f34929e6587c18328b75c
Moncef <moncefelhilali@gmail.com>
2023-12-30 11:51:43 +0100

    Health Check Controller

---
+++ Utilisateurs.Api/Controllers/HealthController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Utilisateurs.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+        public HealthController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CheckHealth()
+        {
+            var result = await _httpClient.GetFromJsonAsync<object?>("https://localhost:7256/_health");
+            return Ok(result);
+        }
+    }
+}
